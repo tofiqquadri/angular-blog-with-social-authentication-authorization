beb96f6df9a38d834abcb5c5ea8c617439f9d384
tofiqquadri <tofiqquadri@ymail.com>
2023-07-20 17:53:21 +0530

    Updated Environment Variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: true,
+    production: false,
     googleClientId:
-        '',
-    baseUrl: '',
+        '956146513529-vigkql3j35ss2t1oq8hb7qtvhcg8va18.apps.googleusercontent.com',
+    baseUrl: 'http://localhost:4200',
     endpointUrl:
-        ''
+        'https://nestjs-blog-with-social-auth-google-authorization.vercel.app'
 };
